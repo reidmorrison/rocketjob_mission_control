57f182a10a2c92065e5568324f7d1e0e671302e4
Chris Lamb <lamb.chrisr@gmail.com>
2015-03-24 10:23:50 -0400

    Move view class logic into a helper.

--- app/helpers/rocket_job_mission_control/jobs_helper.rb
+++ app/helpers/rocket_job_mission_control/jobs_helper.rb
@@ -1,5 +1,20 @@
 module RocketJobMissionControl
   module JobsHelper
+    def job_state_icon(state)
+      case state
+      when :queued
+        'fa-bed warning'
+      when :running
+        'fa-cog fa-spin primary'
+      when :completed
+        'fa-check-circle-o success'
+      when :aborted
+        'fa-times-circle-o warning'
+      else
+        'fa-times-circle-o danger'
+      end
+    end
+
     def job_class(job)
       case job.state
       when :queued
--- spec/helpers/jobs_helper_spec.rb
+++ spec/helpers/jobs_helper_spec.rb
@@ -2,15 +2,32 @@ require 'rails_helper'
 
 module RocketJobMissionControl
   RSpec.describe JobsHelper, type: :helper do
+    describe "#job_state_icon" do
+      {
+        queued:     'fa-bed warning',
+        running:    'fa-cog fa-spin primary',
+        completed:  'fa-check-circle-o success',
+        aborted:    'fa-times-circle-o warning',
+        unexpected: 'fa-times-circle-o danger'
+      }.each do |state, expected_class|
+        context "when the job state is #{state}" do
+          it "returns the correct class" do
+            expect(helper.job_state_icon(state)).to eq(expected_class)
+          end
+        end
+      end
+    end
+
     describe "#job_class" do
       let(:job) { double(:job, state: job_state) }
 
       {
-        queued:    "warning",
-        running:   "primary",
-        completed: "success",
-        aborted:   "warning",
-        failed:    "danger",
+        queued:     "warning",
+        running:    "primary",
+        completed:  "success",
+        aborted:    "warning",
+        failed:     "danger",
+        unexpected: "",
       }.each do |state, expected_class|
         context "when job state is #{state}" do
           let(:job_state) { state }

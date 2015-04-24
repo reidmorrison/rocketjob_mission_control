13afc578974ee1317ef217aef58767b45335ba44
Chris Lamb <lamb.chrisr@gmail.com>
2015-04-24 11:26:57 -0400

    Remove unnecessary ternary.

--- app/helpers/rocket_job_mission_control/application_helper.rb
+++ app/helpers/rocket_job_mission_control/application_helper.rb
@@ -6,7 +6,7 @@ module RocketJobMissionControl
 
     def title
       @page_title ||= params[:controller].to_s.titleize
-      h(@full_title ? @full_title : [@page_title, site_title].compact.join(' | '))
+      h(@full_title || [@page_title, site_title].compact.join(' | '))
     end
 
     def active_page(path)

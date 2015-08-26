6eb9232bd1b8332091389cc1a6ce46d1a9ed3996
Chris Lamb <lamb.chrisr@gmail.com>
2015-08-26 16:25:37 -0400

    Denote zombie workers with a new style.

--- app/helpers/rocket_job_mission_control/workers_helper.rb
+++ app/helpers/rocket_job_mission_control/workers_helper.rb
@@ -2,12 +2,16 @@ module RocketJobMissionControl
   module WorkersHelper
 
     def worker_card_class(worker)
-      map = {
-        running:  'callout-success-top',
-        paused:   'callout-warning-top',
-        stopping: 'callout-alert-top',
-      }
-      map[worker.state] || 'callout-info-top'
+      if worker.zombie?
+        'callout-zombie-top'
+      else
+        map = {
+          running:  'callout-success-top',
+          paused:   'callout-warning-top',
+          stopping: 'callout-alert-top',
+        }
+        map[worker.state] || 'callout-info-top'
+      end
     end
 
   end

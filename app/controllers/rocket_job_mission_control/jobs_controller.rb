0cace6c30b91c335a8b5a3d463d304a1e766079f
Reid Morrison <reidmo@gmail.com>
2016-01-28 16:35:58 -0500

    Replace activity page and display active workers.

--- app/controllers/rocket_job_mission_control/jobs_controller.rb
+++ app/controllers/rocket_job_mission_control/jobs_controller.rb
@@ -5,7 +5,22 @@ module RocketJobMissionControl
     rescue_from StandardError, with: :error_occurred
 
     def running
-      @jobs = RocketJob::Job.where(state: 'running').sort(_id: -1)
+      # The list of workers actively processing jobs
+      # [Array[Array<worker_name [String], job [RocketJob::Job], slice_id [String]]]
+      sorted = true
+      t      = Time.new
+      busy   = []
+      RocketJob::Job.running.sort(:worker_name).collect do |job|
+        if job.respond_to?(:input_categories)
+          sorted = false
+          job.input.each('state' => 'running') do |slice|
+            busy << [slice.worker_name, job, slice.started_at]
+          end
+        else
+          busy << [job.worker_name, job, job.started_at]
+        end
+      end
+      @busy = sorted ? busy : busy.sort_by { |result| result.first }
     end
 
     def update
@@ -59,7 +74,7 @@ module RocketJobMissionControl
 
     def index
       @state = params[:state] || 'running'
-      @jobs = RocketJob::Job.limit(1000).sort(_id: :desc)
+      @jobs  = RocketJob::Job.limit(1000).sort(_id: :desc)
       unless @state == 'all'
         if @state == 'scheduled'
           @jobs = @jobs.scheduled

466495b09105f6a62123e31c409ea4d55b0acab8
Reid Morrison <reidmo@gmail.com>
2016-05-17 16:25:34 -0400

    Fixes for Travis-CI

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,3 @@
-Rails.application.routes.draw do
-
-  mount RocketJobMissionControl::Engine => "/rocket_job_mission_control"
+Dummy::Application.routes.draw do
+  mount RocketJobMissionControl::Engine => '/rocket_job_mission_control'
 end

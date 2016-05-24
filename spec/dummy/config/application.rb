064d9764c001474216ec5f021a7bb7ec08c18ec7
Chris Lamb <lamb.chrisr@gmail.com>
2016-05-24 13:33:55 -0400

    Add Active Record back to dummy app for rails 5 support.
    boo.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,22 +1,26 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+require 'rails/all'
 
-
-Bundler.require
+Bundler.require(*Rails.groups)
+require "rocketjob_mission_control"
 
 module Dummy
   class Application < Rails::Application
-    config.encoding = "utf-8"
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-    config.filter_parameters += [:password]
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
-    config.assets.enabled = true
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
 
-    config.assets.version = '1.0'
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
 

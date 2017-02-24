496ae7db5961d14c7ebc937c2622d453ad6a3d07
Reid Morrison <reidmo@gmail.com>
2017-02-24 15:08:19 -0500

    Convert helper tests from rspec to minitest

---
+++ test/helpers/rocket_job_mission_control/slices_helper_test.rb
@@ -0,0 +1,30 @@
+require_relative '../../test_helper'
+
+module RocketJobMissionControl
+  SlicesHelper.include(RocketJobMissionControl::ApplicationHelper)
+  class SlicesHelperTest < ActionView::TestCase
+    describe SlicesHelper do
+
+      describe '#display_slice_info' do
+        let(:slice) { {id: 42, name: 'test'} }
+
+        describe 'when encrypted' do
+          it 'only shows encrypted text' do
+            assert_equal 'encrypted', display_slice_info(slice, true)
+          end
+        end
+
+        describe 'when not encrypted' do
+          it "does not return 'encrypted'" do
+            refute_equal 'encrypted', display_slice_info([1,2], false)
+          end
+
+          it "displays the slice info" do
+            assert_equal "[<br />  1,<br />  2<br />]", display_slice_info([1,2], false)
+          end
+        end
+
+      end
+    end
+  end
+end

50f49066465f58d0832c06908944f9c0f5c99b41
Reid Morrison <reidmo@gmail.com>
2021-03-29 18:12:05 -0400

    Support Rocket Job v6

--- app/helpers/rocket_job_mission_control/jobs_helper.rb
+++ app/helpers/rocket_job_mission_control/jobs_helper.rb
@@ -1,5 +1,18 @@
 module RocketJobMissionControl
   module JobsHelper
+    # The fields that RJMC already displays, all other will be rendered under the custom section.
+    DISPLAYED_FIELDS = %w[
+      _id _type completed_at created_at
+      description destroy_on_complete download_encryption
+      exception
+      failed_at_list failure_count
+      input_categories
+      output_categories output_path
+      percent_complete priority
+      retry_limit run_at
+      started_at state statistics sub_state
+    ]
+
     def job_icon(job)
       state = job_state(job)
       state_icon(state)
@@ -15,6 +28,46 @@ module RocketJobMissionControl
       end
     end
 
+    def job_state_name(job)
+      job_state(@job).to_s.camelcase
+    end
+
+    def job_time(time)
+      return "" unless time
+      time.in_time_zone(Time.zone)
+    end
+
+    def job_state_time(job)
+      return job_time(job.run_at) if job.scheduled?
+
+      job_time(job.running? ? job.started_at : job.completed_at)
+    end
+
+    def job_estimated_time_left(job)
+      if job.record_count && job.running? && job.record_count.positive?
+        percent = job.percent_complete
+        if percent >= 5
+          secs = job.seconds.to_f
+          RocketJob.seconds_as_duration((((secs / percent) * 100) - secs))
+        end
+      end
+    end
+
+    def job_records_per_hour(job)
+      return unless job.completed?
+
+      secs = job.seconds.to_f
+      ((record_count.to_f / secs) * 60 * 60).round if job.record_count&.positive? && (secs > 0.0)
+    end
+
+    def job_custom_fields(job)
+      attrs = job.attributes.dup
+      DISPLAYED_FIELDS.each { |key| attrs.delete(key) }
+      # Convert time zones for any custom time fields
+      attrs.keys { |key| attrs[key] = attrs[key].in_time_zone(Time.zone) if attrs[key].is_a?(Time) }
+      attrs
+    end
+
     def job_states
       @job_states ||= RocketJob::Job.aasm.states.map { |state| state.name.to_s }
     end
@@ -25,10 +78,10 @@ module RocketJobMissionControl
 
     def job_counts_by_state(state)
       @job_counts ||= begin
-        counts          = RocketJob::Job.counts_by_state
-        counts[:queued] = counts[:queued_now] || 0
-        counts
-      end
+                        counts          = RocketJob::Job.counts_by_state
+                        counts[:queued] = counts[:queued_now] || 0
+                        counts
+                      end
       @job_counts.fetch(state.downcase.to_sym, 0)
     end
 

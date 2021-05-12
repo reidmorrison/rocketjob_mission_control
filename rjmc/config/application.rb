36525cf4e29019746a1c6f9fd686081a7cad8707
Michael Cloutier <viotech@comcast.net>
2021-05-12 17:48:33 -0400

    Remove hosts config.

--- rjmc/config/application.rb
+++ rjmc/config/application.rb
@@ -26,8 +26,5 @@ module Rjmc5
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Allow lvh.me for localhost testing
-    config.hosts << "lvh.me"
   end
 end
